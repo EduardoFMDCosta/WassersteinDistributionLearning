bf5753a4eca25b5d65aeac576c373a41d503a9a6
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-07-13 19:12:07 +0200

    refactor confidence

--- confidence.py
+++ confidence.py
@@ -1,42 +1,44 @@
 import torch
+import math
+from scipy.special import comb
+from scipy.optimize import brentq
 
 class DuchiConfidence:
     def __init__(self,
-                 delta: torch.Tensor,
-                 alpha: torch.Tensor,
-                 num_samples: int):
+                 beta: float,
+                 n_set: int,
+                 n: int):
 
-        assert (0.0 < delta < 1.0).all()
-        assert (0.0 <= alpha <= 1.0).all()
+        assert 0.0 < beta < 1.0
+        assert 0 <= n_set <= n
 
-        self.delta = delta
-        self.alpha = alpha
-        self.num_samples = num_samples
+        self.beta = beta
+        self.empirical_proba = n_set / n
+        self.n = n
 
-        self.gamma = self._get_gamma()
+        self.epsilon = self._get_epsilon()
 
-    def _get_gamma(self):
+    def _get_epsilon(self):
         #See Proposition 2 in Duchi, 2025 (https://arxiv.org/pdf/2503.00220)
 
-        first_term = 4/3 * torch.log(1/self.delta) / self.num_samples
-        second_term = (4/3 * torch.log(1/self.delta) / self.num_samples) ** 2 + 2 * self.alpha * (1 - self.alpha) * torch.log(1/self.delta) / self.num_samples
-
+        first_term = 4/3 * math.log(1/self.beta) / self.n
+        second_term = (4/3 * math.log(1/self.beta) / self.n) ** 2 + 2 * (1 - self.empirical_proba) * self.empirical_proba * math.log(1/self.beta) / self.n
         return first_term + second_term ** 0.5
 
 class HoeffdingConfidence:
     def __init__(self,
-                 delta: torch.Tensor,
-                 alpha: torch.Tensor,
-                 num_samples: int):
+                 beta: float,
+                 n_set: int,
+                 n: int):
 
-        assert (0.0 < delta < 1.0).all()
-        assert (0.0 <= alpha <= 1.0).all()
+        assert 0.0 < beta < 1.0
+        assert 0 <= n_set <= n
 
-        self.delta = delta
-        self.alpha = alpha
-        self.num_samples = num_samples
+        self.beta = beta
+        self.empirical_proba = n_set / n
+        self.n = n
 
-        self.gamma = self._get_gamma()
+        self.epsilon = self._get_epsilon()
 
-    def _get_gamma(self):
-        return (torch.log(2/self.delta) / (2 * self.num_samples)) ** 0.5
+    def _get_epsilon(self):
+        return (math.log(2/self.beta) / (2 * self.n)) ** 0.5

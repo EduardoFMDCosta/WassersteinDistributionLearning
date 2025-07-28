e991ee9826fbccf1595770800e705e86928c0d79
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-07-28 19:29:39 +0200

    add refinement to experiment

--- experiments/compare_ambiguity_set_radius.py
+++ experiments/compare_ambiguity_set_radius.py
@@ -4,25 +4,27 @@ from bound import data_driven_radius, fournier_radius
 from sets import HyperRectangle, Partition
 from distributions import Uniform
 from plotting.plot import plot_samples
-from utils import generate_grid
+from utils import generate_grid_from_samples
 
 if __name__ == '__main__':
     torch.manual_seed(0)
 
-    M = 100
+    M = 10
     N = 2000
     beta = 1e-4
 
     support_assumption = HyperRectangle(lower=torch.tensor([-0.5, -0.5]), upper=torch.tensor([0.5, 0.5]))
 
     # (Unknown) Generating probability
-    support = HyperRectangle(lower=torch.tensor([0.1, 0.1]), upper=torch.tensor([0.15, 0.15]))
+    support = HyperRectangle(lower=torch.tensor([-0.15, -0.15]), upper=torch.tensor([0.15, 0.15]))
     distribution = Uniform(support=support)
     samples = distribution(num_samples=N)
 
     # Clusterize samples (obtaining \hat{P}_M)
-    locs = generate_grid(samples, int(M ** 0.5))
+    locs = generate_grid_from_samples(samples, int(M ** 0.5))
     partition = Partition(locs=locs, support=support_assumption)
+    for i in range(1):
+        partition = partition.refine(samples=samples, prob_thr=0.01, diam_thr=0.1)
 
     # Plot samples and clusterized distribution
     plot_samples(samples=samples, regions=partition.regions, support_assumption=support_assumption)

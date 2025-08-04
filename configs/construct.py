8287dc834b39fa7ee905df32da72f02f524648c5
sjladams <steven.adams.95@hotmail.com>
2025-08-04 09:41:44 +0200

    style: rename covariance_matrix -> variance

--- configs/construct.py
+++ configs/construct.py
@@ -24,20 +24,20 @@ def get_support(distribution, support, **kwargs):
 def construct_uniform(support, support_assumption, **kwargs) -> MultivariateUniform:
     return MultivariateUniform(low=torch.as_tensor(support[0]), high=torch.as_tensor(support[1]))
 
-def construct_trunc_mult_norm(mean, covariance_matrix, support_assumption, **kwargs) -> TruncatedMultivariateNormal:
+def construct_trunc_mult_norm(mean, variance, support_assumption, **kwargs) -> TruncatedMultivariateNormal:
     return TruncatedMultivariateNormal(
         loc=torch.as_tensor(mean), 
-        scale=torch.as_tensor(covariance_matrix) ** 0.5,
+        scale=torch.as_tensor(variance) ** 0.5,
         a=torch.as_tensor(support_assumption[0]),
         b=torch.as_tensor(support_assumption[1])
     )
 
-def construct_mixture_trunc_mult_norm(weight, mean, covariance_matrix, support_assumption, **kwargs) -> MixtureTruncatedMultivariateNormal:
+def construct_mixture_trunc_mult_norm(weight, mean, variance, support_assumption, **kwargs) -> MixtureTruncatedMultivariateNormal:
     mixture_distribution = torch.distributions.Categorical(probs=torch.as_tensor(weight))
 
     component_distribution = TruncatedMultivariateNormal(
         loc=torch.as_tensor(mean), 
-        scale=torch.tensor(covariance_matrix) ** 0.5,
+        scale=torch.tensor(variance) ** 0.5,
         a=torch.as_tensor(support_assumption[0]).expand(len(mean), -1),
         b=torch.as_tensor(support_assumption[1]).expand(len(mean), -1)
     )

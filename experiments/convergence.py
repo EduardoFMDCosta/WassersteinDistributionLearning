56f0cadff08fb308b1c0845a97aaef275bfcefb1
sjladams <steven.adams.95@hotmail.com>
2025-08-05 16:05:09 +0200

    feat: extend convergence.py to num_samples

--- experiments/convergence.py
+++ experiments/convergence.py
@@ -9,34 +9,18 @@ from configs.handlers import parse_arguments
 
 import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-    torch.manual_seed(0)
 
-    args = parse_arguments(
-        distribution="Discrete",
-        dimension=2,
-        setting=0,
-        num_samples=5000,
-        num_clusters=None,
-        beta=1e-4,
-        plot=False
-    )
-
-    beta = args.beta
-    method = 'dual_sinkhorn'
+def num_samples(args, M):
     support_assumption = get_support_assumption(**vars(args))
-    # M = args.num_clusters
-    N = args.num_samples
 
-    # (Unknown) Generating probability
     distribution = get_distribution(**vars(args))
-
-    # Generate samples
-    samples = distribution.sample((N,))
     
     data_driven_bounds, data_driven_lower_bounds, fournier_bounds = list(), list(), list()
-    M_options = torch.arange(20, 100, 10).tolist()
-    for M in M_options:
+    N_options = [1000, 5000, 10000, 50000]
+    for N in N_options:
+        print(f"Number of clusters (M) / num_samples (N): {M} / {N}")
+        samples = distribution.sample((N,))
+
         # Clusterize samples (obtaining \hat{P}_M)
         partition = KMeansPartition(support=support_assumption, samples=samples, k=int(M), prefilter=args.distribution == "Discrete")
 
@@ -45,21 +29,65 @@ if __name__ == '__main__':
             plot_kmeans_partition(partition=partition)
 
         # Compute bounds
-        data_driven_output = data_driven_radius(partition=partition, beta=beta, method=method)
+        data_driven_output = data_driven_radius(partition=partition, beta=args.beta, method=args.method)
         data_driven_bounds.append(data_driven_output.radius)
         data_driven_lower_bounds.append(data_driven_output.lower_bound)
 
-        fournier_bound = fournier_radius(partition=partition, beta=beta)
-        fournier_bounds.append(fournier_bound)
+        fournier_bounds.append(fournier_radius(partition=partition, beta=args.beta))
 
-        print(f"Number of clusters (M) / num_samples (N): {M} / {N} \n"
-            f"\t Ours: {data_driven_output.radius:.4f} \n"
-            f"\t Fournier: {fournier_bound:.4f} \n")
+    return N_options, data_driven_bounds, fournier_bounds, data_driven_lower_bounds
+
+def num_clusters(args, N):
+    support_assumption = get_support_assumption(**vars(args))
+
+    distribution = get_distribution(**vars(args))
+    samples = distribution.sample((N,))
+    
+    data_driven_bounds, data_driven_lower_bounds, fournier_bounds = list(), list(), list()
+    M_options = torch.arange(20, 100, 10).tolist()
+    for M in M_options:
+        print(f"Number of clusters (M) / num_samples (N): {M} / {N}")
+        # Clusterize samples (obtaining \hat{P}_M)
+        partition = KMeansPartition(support=support_assumption, samples=samples, k=int(M), prefilter=args.distribution == "Discrete")
+
+        # Plot samples and clusterized distribution
+        if args.plot:
+            plot_kmeans_partition(partition=partition)
+
+        # Compute bounds
+        data_driven_output = data_driven_radius(partition=partition, beta=args.beta, method=args.method)
+        data_driven_bounds.append(data_driven_output.radius)
+        data_driven_lower_bounds.append(data_driven_output.lower_bound)
+
+        fournier_bounds.append(fournier_radius(partition=partition, beta=args.beta))
+
+    return M_options, data_driven_bounds, fournier_bounds, data_driven_lower_bounds
+
+if __name__ == '__main__':
+    torch.manual_seed(0)
+
+    args = parse_arguments(
+        distribution="Discrete",
+        dimension=2,
+        setting=0,
+        num_samples=5000,
+        num_clusters=40,
+        beta=1e-4,
+        plot=False
+    )
+    args.method = 'dual_sinkhorn'
+    investigate_clusters = False
+
+    if investigate_clusters:
+        options, data_driven_bounds, fournier_bounds, data_driven_lower_bounds = num_clusters(args, N=args.num_samples)
+    else:
+        options, data_driven_bounds, fournier_bounds, data_driven_lower_bounds = num_samples(args, M=args.num_clusters)
 
     with torch.no_grad():
-        plt.plot(M_options, torch.tensor(data_driven_bounds), label='Ours', marker='o')
-        plt.plot(M_options, torch.tensor(fournier_bounds), label='Fournier', marker='x')
-        plt.plot(M_options, torch.tensor(data_driven_lower_bounds), label='Ours Lower Bound', linestyle='--')
-        plt.xlabel("Number of clusters (M)")
+        plt.plot(options, torch.tensor(data_driven_bounds), label='Ours', marker='o')
+        plt.plot(options, torch.tensor(fournier_bounds), label='Fournier', marker='x')
+        plt.plot(options, torch.tensor(data_driven_lower_bounds), label='Ours Lower Bound', linestyle='--')
+        plt.xlabel(f"Number of {'clusters (M)' if investigate_clusters else 'samples (N)'}")
+        plt.title(f"Number of {'samples (N)' if investigate_clusters else 'clusters (M)'} = {args.num_samples if investigate_clusters else args.num_clusters}")
         plt.legend()
         plt.show()

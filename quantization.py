881247aeb4c670e2333c008e712d197f723c9789
sjladams <steven.adams.95@hotmail.com>
2025-08-13 15:28:27 +0200

    fix: assign rest count to outer region of quantization

--- quantization.py
+++ quantization.py
@@ -1,22 +1,27 @@
 import torch
 
-from sets import Partition
+from sets import Partition, VoronoiPartition
 
 
 class Quantization:
     def __init__(self, partition: Partition, samples: torch.Tensor):
-
         self.partition = partition
         self.samples = samples
         self.nsamples = samples.size(0)
 
-        distances = torch.cdist(partition.cluster_centers, samples, p=2)
-        assignment = torch.argmin(distances, dim=0)
-        self.cluster_counts = torch.bincount(assignment, minlength=partition.cluster_centers.size(0))
+        if isinstance(partition, VoronoiPartition):
+            distances = torch.cdist(partition.cluster_centers, samples, p=2)
+            assignment = torch.argmin(distances, dim=0)
+            self.cluster_counts = torch.bincount(assignment, minlength=partition.cluster_centers.size(0))
+        else:
+            raise NotImplementedError
+    
+        self.outer_counts = self.nsamples - self.cluster_counts.sum()
+        assert self.outer_counts >= 0
 
     @property
     def counts(self):
-        return torch.cat((self.cluster_counts, torch.zeros(1)))
+        return torch.cat((self.cluster_counts, self.outer_counts.view(1)))
 
     @property
     def probs(self):

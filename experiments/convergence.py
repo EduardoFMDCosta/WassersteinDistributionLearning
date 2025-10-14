def1c2baaf3bc2f0856e29fe7f1fe4793656a420
sjladams <steven.adams.95@hotmail.com>
2025-10-14 20:46:08 +0200

    move convergence plotting functions to plot.py

--- experiments/convergence.py
+++ experiments/convergence.py
@@ -13,6 +13,7 @@ from configs.construct import get_support_assumption, get_distribution
 from configs.handlers import parse_arguments
 
 import matplotlib.pyplot as plt
+import plotting.plot as plot
 
 
 def run_combinations(args, M_options, N_options):
@@ -54,93 +55,6 @@ def run_combinations(args, M_options, N_options):
 
     return quantizations, data_driven_radii, fournier_radii
 
-@torch.no_grad()
-def plot_w2_slice(
-    ax, 
-    data_driven_radii: DataDrivenRadii, 
-    N: Optional[int] = None, 
-    M: Optional[int] = None
-):
-    if (N is not None and M is not None) or (N is None and M is None):
-        raise ValueError("Only N or M should be specified.")
-
-    data_sliced = data_driven_radii._slice(N=N, M=M)
-    idx = 1 if N is not None else 0
-    options = [key[idx] for key in data_driven_radii.keys()]
-
-    ax.plot(options, data_sliced.radius, label='w2', marker='o')
-    ax.plot(options, data_sliced.moment_bound, label='e1', linestyle='--')
-    ax.plot(options, data_sliced.discrete_bound, label='e2', linestyle=':')
-    ax.plot(options, data_sliced.lower_bound, label='lower_bound', linestyle='--')
-    ax.set_xlabel(f"Number of {'clusters (M)' if M is None else 'samples (N)'}")
-    ax.set_title(f"Number of {'samples (N) = ' if M is None else 'clusters (M)'} = {N if M is None else M}")
-    # ax.set_xscale('log')
-    ax.legend(loc='best')
-    return ax
-
-@torch.no_grad()
-def plot_quantization_slice(
-    ax, 
-    quantizations: Quantizations, 
-    stat: str,
-    N: Optional[int] = None, 
-    M: Optional[int] = None
-):
-    if (N is not None and M is not None) or (N is None and M is None):
-        raise ValueError("Only N or M should be specified.")
-    
-    data_sliced = quantizations._slice(N=N, M=M)
-    idx = 1 if N is not None else 0
-    options = [key[idx] for key in quantizations.keys()]
-
-    if stat == 'probs':
-        ax.plot(options, data_sliced.mean_range_probs, label='avg probs range', color='black')
-        ax.fill_between(
-            options,
-            data_sliced.mean_range_probs - data_sliced.std_range_probs,
-            data_sliced.mean_range_probs + data_sliced.std_range_probs,
-            color='black',
-            alpha=0.2,
-            label='std dev'
-        )
-    elif stat == 'radii':
-        ax.plot(options, data_sliced.mean_cluster_radii, label='avg cluster radii', color='black')
-        ax.fill_between(
-            options,
-            data_sliced.mean_cluster_radii - data_sliced.std_cluster_radii,
-            data_sliced.mean_cluster_radii + data_sliced.std_cluster_radii,
-            color='black',
-            alpha=0.2,
-            label='std dev'
-        )
-    elif stat == 'counts':    
-        ax.plot(options, data_sliced.outer_counts, label='outer counts', marker='o', color='red')
-        ax.plot(options, data_sliced.mean_cluster_counts, label='avg cluster counts', marker='o', color='blue')
-        ax.fill_between(
-            options,
-            data_sliced.mean_cluster_counts - data_sliced.std_cluster_counts,
-            data_sliced.mean_cluster_counts + data_sliced.std_cluster_counts,
-            color='blue',
-            alpha=0.2,
-            label='std dev'
-        )
-    elif stat == 'locs':
-        ax.plot(options, data_sliced.mean_distances_locs, label='avg distances', color='black')
-        ax.fill_between(
-            options,
-            data_sliced.mean_distances_locs - data_sliced.std_distances_locs,
-            data_sliced.mean_distances_locs + data_sliced.std_distances_locs,
-            color='black',
-            alpha=0.2,
-            label='std dev'
-        )
-    else:
-        raise ValueError(f"Stat {stat} not recognized. Choose from 'probs', 'radii', 'counts', 'locs'.")
-    
-    # ax.set_xscale('log')
-    ax.legend(loc='best')
-    return ax
-
 
 if __name__ == '__main__':
     torch.manual_seed(0)
@@ -169,11 +83,11 @@ if __name__ == '__main__':
 
     fig, ax = plt.subplots(4, 1, figsize=(8, 12), constrained_layout=True)
 
-    ax[0] = plot_w2_slice(ax[0], data_driven_radii, N=N_options[0])
-    ax[1] = plot_quantization_slice(ax[1], quantizations, stat='probs', N=N_options[0])
-    ax[2] = plot_quantization_slice(ax[2], quantizations, stat='radii', N=N_options[0])
-    ax[3] = plot_quantization_slice(ax[3], quantizations, stat='counts', N=N_options[0])
-    # ax[4] = plot_quantization_slice(ax[4], quantizations, stat='locs', N=N_options[0])
+    ax[0] = plot.plot_w2_slice(ax[0], data_driven_radii, N=N_options[0])
+    ax[1] = plot.plot_quantization_slice(ax[1], quantizations, stat='probs', N=N_options[0])
+    ax[2] = plot.plot_quantization_slice(ax[2], quantizations, stat='radii', N=N_options[0])
+    ax[3] = plot.plot_quantization_slice(ax[3], quantizations, stat='counts', N=N_options[0])
+    # ax[4] = plot.plot_quantization_slice(ax[4], quantizations, stat='locs', N=N_options[0])
 
     tag = f"convergence_{args.distribution}_setting={args.setting}"
     if investigate_clusters:
--- plotting/plot.py
+++ plotting/plot.py
@@ -9,6 +9,7 @@ from quantization import Quantization
 from sets import BoundedVoronoiPartition
 from confidence import Confidence
 import plotting.utils_plot as utils_plot
+from experiments.utils import DataDrivenRadii, Quantizations
 
 colors = [
         "lightcoral",
@@ -23,6 +24,95 @@ plt.rcParams.update({
     'text.usetex': True,
     'text.latex.preamble': r'\usepackage{amsfonts}'
 })
+
+
+@torch.no_grad()
+def plot_w2_slice(
+    ax, 
+    data_driven_radii: DataDrivenRadii, 
+    N: Optional[int] = None, 
+    M: Optional[int] = None
+):
+    if (N is not None and M is not None) or (N is None and M is None):
+        raise ValueError("Only N or M should be specified.")
+
+    data_sliced = data_driven_radii._slice(N=N, M=M)
+    idx = 1 if N is not None else 0
+    options = [key[idx] for key in data_driven_radii.keys()]
+
+    ax.plot(options, data_sliced.radius, label='w2', marker='o')
+    ax.plot(options, data_sliced.moment_bound, label='e1', linestyle='--')
+    ax.plot(options, data_sliced.discrete_bound, label='e2', linestyle=':')
+    ax.plot(options, data_sliced.lower_bound, label='lower_bound', linestyle='--')
+    ax.set_xlabel(f"Number of {'clusters (M)' if M is None else 'samples (N)'}")
+    ax.set_title(f"Number of {'samples (N) = ' if M is None else 'clusters (M)'} = {N if M is None else M}")
+    # ax.set_xscale('log')
+    ax.legend(loc='best')
+    return ax
+
+@torch.no_grad()
+def plot_quantization_slice(
+    ax, 
+    quantizations: Quantizations, 
+    stat: str,
+    N: Optional[int] = None, 
+    M: Optional[int] = None
+):
+    if (N is not None and M is not None) or (N is None and M is None):
+        raise ValueError("Only N or M should be specified.")
+    
+    data_sliced = quantizations._slice(N=N, M=M)
+    idx = 1 if N is not None else 0
+    options = [key[idx] for key in quantizations.keys()]
+
+    if stat == 'probs':
+        ax.plot(options, data_sliced.mean_range_probs, label='avg probs range', color='black')
+        ax.fill_between(
+            options,
+            data_sliced.mean_range_probs - data_sliced.std_range_probs,
+            data_sliced.mean_range_probs + data_sliced.std_range_probs,
+            color='black',
+            alpha=0.2,
+            label='std dev'
+        )
+    elif stat == 'radii':
+        ax.plot(options, data_sliced.mean_cluster_radii, label='avg cluster radii', color='black')
+        ax.fill_between(
+            options,
+            data_sliced.mean_cluster_radii - data_sliced.std_cluster_radii,
+            data_sliced.mean_cluster_radii + data_sliced.std_cluster_radii,
+            color='black',
+            alpha=0.2,
+            label='std dev'
+        )
+    elif stat == 'counts':    
+        ax.plot(options, data_sliced.outer_counts, label='outer counts', marker='o', color='red')
+        ax.plot(options, data_sliced.mean_cluster_counts, label='avg cluster counts', marker='o', color='blue')
+        ax.fill_between(
+            options,
+            data_sliced.mean_cluster_counts - data_sliced.std_cluster_counts,
+            data_sliced.mean_cluster_counts + data_sliced.std_cluster_counts,
+            color='blue',
+            alpha=0.2,
+            label='std dev'
+        )
+    elif stat == 'locs':
+        ax.plot(options, data_sliced.mean_distances_locs, label='avg distances', color='black')
+        ax.fill_between(
+            options,
+            data_sliced.mean_distances_locs - data_sliced.std_distances_locs,
+            data_sliced.mean_distances_locs + data_sliced.std_distances_locs,
+            color='black',
+            alpha=0.2,
+            label='std dev'
+        )
+    else:
+        raise ValueError(f"Stat {stat} not recognized. Choose from 'probs', 'radii', 'counts', 'locs'.")
+    
+    # ax.set_xscale('log')
+    ax.legend(loc='best')
+    return ax
+
 
 @torch.no_grad()
 def plot_confidence(nums_samples:list,

758c59058fb5e12d1fca6252333834cf77956ede
sjladams <steven.adams.95@hotmail.com>
2025-10-15 09:09:12 +0200

    refactor: construct support by linf radius

--- configs/construct.py
+++ configs/construct.py
@@ -6,42 +6,60 @@ from sets import HyperRectangle
 
 def get_support_assumption(
     num_dims: int,
-    support: Optional[List[List[float]]] = None, 
+    support_linf_radius: Optional[float] = None, 
     support_linf_radius_assumed: Optional[float] = None, 
     **kwargs
 ):
     if support_linf_radius_assumed is not None:
         return HyperRectangle.from_eps(x=torch.zeros(num_dims), eps=support_linf_radius_assumed)
-    elif support is not None:
-        return HyperRectangle(lower=torch.as_tensor(support[0]), upper=torch.as_tensor(support[1]))
+    elif support_linf_radius is not None:
+        return HyperRectangle.from_eps(x=torch.zeros(num_dims), eps=torch.as_tensor(support_linf_radius))
     else:
         raise ValueError("Either 'support' or 'support_linf_radius_assumed' must be provided.")
     
-def construct_uniform(support, **kwargs) -> MultivariateUniform:
-    return MultivariateUniform(low=torch.as_tensor(support[0]), high=torch.as_tensor(support[1]))
+def construct_uniform(
+    num_dims: int,
+    support_linf_radius: float, 
+    **kwargs
+) -> MultivariateUniform:
+    return MultivariateUniform(
+        low=torch.ones(num_dims) * -support_linf_radius, 
+        high=torch.ones(num_dims) * support_linf_radius
+    )
 
-def construct_trunc_mult_norm(mean, variance, support, **kwargs) -> TruncatedMultivariateNormal:
+def construct_trunc_mult_norm(
+    num_dims: int,
+    mean, 
+    variance, 
+    support_linf_radius: float, 
+    **kwargs
+) -> TruncatedMultivariateNormal:
     return TruncatedMultivariateNormal(
         loc=torch.as_tensor(mean), 
         scale=torch.as_tensor(variance) ** 0.5,
-        a=torch.as_tensor(support[0]),
-        b=torch.as_tensor(support[1])
+        a=torch.ones(num_dims) * -support_linf_radius,
+        b=torch.ones(num_dims) * support_linf_radius
     )
 
-def construct_mixture_trunc_mult_norm(weight, mean, variance, support, **kwargs) -> MixtureTruncatedMultivariateNormal:
+def construct_mixture_trunc_mult_norm(
+    num_dims: int,
+    weight, 
+    mean, 
+    variance, 
+    support_linf_radius: float, 
+    **kwargs
+) -> MixtureTruncatedMultivariateNormal:
     mixture_distribution = torch.distributions.Categorical(probs=torch.as_tensor(weight))
-
     component_distribution = TruncatedMultivariateNormal(
         loc=torch.as_tensor(mean), 
         scale=torch.tensor(variance) ** 0.5,
-        a=torch.as_tensor(support[0]).expand(len(mean), -1),
-        b=torch.as_tensor(support[1]).expand(len(mean), -1)
+        a=torch.ones(num_dims) * -support_linf_radius,
+        b=torch.ones(num_dims) * support_linf_radius
     )
-
     return MixtureTruncatedMultivariateNormal(mixture_distribution=mixture_distribution, component_distribution=component_distribution)
 
 def construct_random_categorical_float(
-    support_linf_radius_assumed: float, 
+    support_linf_radius_assumed: float,      # TODO use support_linf_radius here?
     support_size: int, 
     num_dims: int,
     **kwargs

2426d26c7fe651f50ee0ddc8723262eefe54f455
sjladams <steven.adams.95@hotmail.com>
2025-11-05 22:59:44 +0100

    update scheduler

--- schedulers/partitions.sh
+++ schedulers/partitions.sh
@@ -1,4 +1,4 @@
 echo "START"
 python -m experiments.partition --distribution Gaussian --num_dims 10 --setting 0
 python -m experiments.partition --distribution Gaussian --num_dims 100 --setting 0
-echo "FINNISHED
+echo "FINNISHED"

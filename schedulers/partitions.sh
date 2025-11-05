7f797fba2ad94aac6211ca77e095a65d386a6bca
sjladams <steven.adams.95@hotmail.com>
2025-11-05 23:14:16 +0100

    update schedulur

--- schedulers/partitions.sh
+++ schedulers/partitions.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 export PYTHONPATH="$PWD${PYTHONPATH:+:$PYTHONPATH}"
 
 echo "START"
-python -u experiments/partition --distribution Gaussian --num_dims 10 --setting 0
+python -u experiments/partition.py --distribution Gaussian --num_dims 10 --setting 0
 echo "FINNISHED"

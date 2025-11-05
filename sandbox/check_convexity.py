e0e8ebec897b5d53038668288060a0498f564adc
sjladams <steven.adams.95@hotmail.com>
2025-11-05 09:06:41 +0100

    refactor: rename legacy -> triangle_inequality

--- sandbox/check_convexity.py
+++ sandbox/check_convexity.py
@@ -1,6 +1,6 @@
 import torch
 
-from solvers.legacy.max_oracle_gradient_descent import inner_lp_maximization
+from solvers.moment_bound_solvers.max_oracle_gradient_descent import inner_lp_maximization
 from quantization import UncertainQuantization
 from sets import BoundedVoronoiPartition
 

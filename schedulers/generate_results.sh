6fe268926faff9b9b15cb433b7b94b428ef988cc
sjladams <steven.adams.95@hotmail.com>
2025-11-06 23:22:37 +0100

    update setup

--- schedulers/generate_results.sh
+++ schedulers/generate_results.sh
@@ -11,8 +11,7 @@ conda activate concentration_inequalities
 export PYTHONPATH="$PWD${PYTHONPATH:+:$PYTHONPATH}"
 
 
-
-for rho in 1; do
+for rho in 1 2; do
 echo "---------------- W${rho} -------------------"
 for method in stochastic_vertice_ascent \
     joint_optimization_milp \
@@ -27,36 +26,36 @@ echo "-- Uniform --"
     echo "dim = 2, setting = 0"
     python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 0 --wasserstein_order "$rho" --method "$method"
 
-#     echo "dim = 2, setting = 1"
-#     python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 1 --wasserstein_order "$rho" --method "$method"
+    echo "dim = 2, setting = 1"
+    python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 1 --wasserstein_order "$rho" --method "$method"
 
-#     echo "dim = 2, setting = 2"
-#     python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 2 --wasserstein_order "$rho" --method "$method"
+    echo "dim = 2, setting = 2"
+    python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 2 --wasserstein_order "$rho" --method "$method"
 
-#     echo "dim = 3, setting = 0"
-#     python -m experiments.generate_results --distribution Uniform --num_dims 3 --setting 0 --wasserstein_order "$rho" --method "$method"
-
-
-# echo "-- Gaussian --"
-#     echo "dim = 2, setting = 0"
-#     python -m experiments.generate_results --distribution Gaussian --num_dims 2 --setting 0 --wasserstein_order "$rho" --method "$method"
-
-#     echo "dim = 3, setting = 0"
-#     python -m experiments.generate_results --distribution Gaussian --num_dims 3 --setting 0 --wasserstein_order "$rho" --method "$method"
-
-#     echo "dim = 10, setting = 0"
-#     python -m experiments.generate_results --distribution Gaussian --num_dims 10 --setting 0 --wasserstein_order "$rho" --method "$method"
-
-#     echo "dim = 100, setting = 0"
-#     python -m experiments.generate_results --distribution Gaussian --num_dims 100 --setting 0 --wasserstein_order "$rho" --method "$method"
+    echo "dim = 3, setting = 0"
+    python -m experiments.generate_results --distribution Uniform --num_dims 3 --setting 0 --wasserstein_order "$rho" --method "$method"
 
 
-# echo "-- GaussianMixture --"
-#     echo "dim = 2, setting = 0"
-#     python -m experiments.generate_results --distribution GaussianMixture --num_dims 2 --setting 0 --wasserstein_order "$rho" --method "$method"
+echo "-- Gaussian --"
+    echo "dim = 2, setting = 0"
+    python -m experiments.generate_results --distribution Gaussian --num_dims 2 --setting 0 --wasserstein_order "$rho" --method "$method"
 
-#     echo "dim = 3, setting = 0"
-#     python -m experiments.generate_results --distribution GaussianMixture --num_dims 3 --setting 0 --wasserstein_order "$rho" --method "$method"
+    echo "dim = 3, setting = 0"
+    python -m experiments.generate_results --distribution Gaussian --num_dims 3 --setting 0 --wasserstein_order "$rho" --method "$method"
+
+    echo "dim = 10, setting = 0"
+    python -m experiments.generate_results --distribution Gaussian --num_dims 10 --setting 0 --wasserstein_order "$rho" --method "$method"
+
+    echo "dim = 100, setting = 0"
+    python -m experiments.generate_results --distribution Gaussian --num_dims 100 --setting 0 --wasserstein_order "$rho" --method "$method"
+
+
+echo "-- GaussianMixture --"
+    echo "dim = 2, setting = 0"
+    python -m experiments.generate_results --distribution GaussianMixture --num_dims 2 --setting 0 --wasserstein_order "$rho" --method "$method"
+
+    echo "dim = 3, setting = 0"
+    python -m experiments.generate_results --distribution GaussianMixture --num_dims 3 --setting 0 --wasserstein_order "$rho" --method "$method"
 
 done        
 done

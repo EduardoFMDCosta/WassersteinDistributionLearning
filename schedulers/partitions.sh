7a483a35b41c6d9c2b67c90a768d02884b727f7d
sjladams <steven.adams.95@hotmail.com>
2025-11-06 20:49:20 +0100

    update setup

--- schedulers/partitions.sh
+++ schedulers/partitions.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Initialize conda
+source /home/sjladams/miniconda3/etc/profile.d/conda.sh
+
+# Activate your environment
 conda activate concentration_inequalities
 
 # optional but explicit

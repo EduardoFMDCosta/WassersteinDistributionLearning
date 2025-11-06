19a4bfedbaf3b845c30ad9267b974b5f3705c87f
sjladams <steven.adams.95@hotmail.com>
2025-11-06 17:14:20 +0100

    initiate generate restuls scheduler

---
+++ schedulers/generate_results.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# optional but explicit
+export PYTHONPATH="$PWD${PYTHONPATH:+:$PYTHONPATH}"
+
+## Solvers to test:
+# stochastic_vertice_ascent
+# joint_optimization_milp
+# diagonal_constrained_tp
+# triangle_inequality_vertex
+# no_triangle_inequality
+# scalar_strategy
+
+echo "-- Uniform --"
+echo "dim = 2, setting = 0"
+python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 0 --method stochastic_vertice_ascent
+python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 0 --method joint_optimization_milp
+python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 0 --method diagonal_constrained_tp 
+python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 0 --method triangle_inequality_vertex 
+python -m experiments.generate_results  --distribution Uniform --num_dims 2 --setting 0 --method scalar_strategy
+
+echo "dim = 2, setting = 1"
+echo "dim = 2, setting = 2"
+echo "dim = 3, setting = 0"
+
+
+echo "-- Gaussian --"
+echo "dim = 2, setting = 0"
+echo "dim = 3, setting = 0"
+echo "dim = 10, setting = 0"
+echo "dim = 100, setting = 0"
+
+
+echo "-- GaussianMixture --"
+echo "dim = 2, setting = 0"
+echo "dim = 3, setting = 0"
+
+

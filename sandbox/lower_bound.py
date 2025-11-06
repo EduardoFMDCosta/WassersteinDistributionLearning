3703c58c119f3580b7cdb9990046a7cf341ab9c0
sjladams <steven.adams.95@hotmail.com>
2025-11-06 13:44:34 +0100

    feat: setup analysis diff full search vs svs discrete solvers

---
+++ sandbox/lower_bound.py
@@ -0,0 +1,63 @@
+import torch
+import matplotlib.pyplot as plt
+
+from quantization import UncertainQuantization
+from bound import DataDrivenRadius
+from solvers import IndependentSolver
+from solvers.discrete_solvers import FullSearch, StochasticVerticeAscent
+
+from configs.handlers import parse_arguments
+from configs.construct import get_support_assumption, get_distribution
+from experiments.partitions import get_partition
+from experiments.datastructures import DataDrivenRadii
+
+def num_vertices_omega_space_vertices(num_dims: int):
+    return num_dims * (2 ** (num_dims - 1))
+
+if __name__ == '__main__':
+    torch.manual_seed(0)
+
+    args = parse_arguments(
+        distribution="Gaussian",
+        num_dims=2,
+        setting=0,
+        num_samples=1000,
+        num_samples_training=1000,
+        compute_discrete_bound=True,
+        compute_moment_bound=False,
+    )
+
+    support_assumption = get_support_assumption(**vars(args))
+    distribution = get_distribution(**vars(args))
+    samples_quantization = distribution.sample((args.num_samples,))
+
+    fs_radii = DataDrivenRadii()
+    sva_radii = DataDrivenRadii()
+
+    num_steps = 5
+    ratio = 0.5
+    
+    num_cluster_options = [5, 6]
+    for num_clusters in num_cluster_options:
+        max_vertices = num_vertices_omega_space_vertices(num_clusters)
+        print(f"vertices = {max_vertices}")
+
+        fs_solver = IndependentSolver(discrete_solver=FullSearch(max_vertices=max_vertices))
+        sva_solver = IndependentSolver(discrete_solver=StochasticVerticeAscent(num_inits=int(max_vertices * ratio / num_steps), num_steps=num_steps))
+
+        partition = get_partition(args=args, num_samples=args.num_samples_training, num_clusters=num_clusters)    
+        quantization = UncertainQuantization(partition=partition, samples=samples_quantization, beta=args.beta)
+
+        # # Compute bounds
+        fs_radii.append((args.num_samples, num_clusters), DataDrivenRadius(quantization=quantization, solver=fs_solver))
+        sva_radii.append((args.num_samples, num_clusters), DataDrivenRadius(quantization=quantization, solver=sva_solver))
+
+
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.plot(num_cluster_options, fs_radii.discrete_bound, label='Full Search')
+    ax.plot(num_cluster_options, sva_radii.discrete_bound, label='Stochastic Vertice Ascent')
+    ax.legend()
+
+    plt.show()
+    pass
+

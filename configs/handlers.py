d63efd44a15dcb3af8256ec0e2163641348d2a14
sjladams <steven.adams.95@hotmail.com>
2025-11-07 13:03:34 +0100

    fix: update results and data-driven radii file paths in parse_arguments function

--- configs/handlers.py
+++ configs/handlers.py
@@ -73,18 +73,19 @@ def parse_arguments(
     args.__dict__.update(vars(dynamics_params))
 
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    args.results_dir = os.path.join(base_dir, "results", f"W{args.wasserstein_order}", args.distribution.lower(), f"dims_{args.num_dims}", f"setting_{args.setting}", args.method)
+    args.results_dir = os.path.join(base_dir, "results", f"W{args.wasserstein_order}", args.distribution.lower(), f"dims_{args.num_dims}", f"setting_{args.setting}")
+
     args.quantizations_file = os.path.join(args.results_dir, "quantizations.pickle")
-    args.data_driven_radii_file = os.path.join(args.results_dir, "data_driven_radii.pickle")
+    args.data_driven_radii_file = os.path.join(args.results_dir, args.method, "data_driven_radii.pickle")
     args.fournier_radii_file = os.path.join(args.results_dir, "fournier_radii.pickle")
     args.empirical_radii_file = os.path.join(args.results_dir, "empirical_radii.pickle")
 
-    # args.figures_dir = os.path.join(base_dir, "figures", args.distribution.lower(), f"dims_{args.num_dims}", f"setting_{args.setting}")
     args.figures_dir = os.path.join(base_dir, "figures")
 
     args.partitions_file = os.path.join(base_dir, "partitions", args.distribution.lower(), f"dims={args.num_dims}_setting={args.setting}.pickle")
 
     ensure_dir(args.results_dir)
+    ensure_dir(os.path.dirname(args.data_driven_radii_file))
     ensure_dir(os.path.dirname(args.partitions_file))
     ensure_dir(args.figures_dir)
 

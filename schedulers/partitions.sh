313c87fcba9a639ee825e1620ad4f40f59530e3c
sjladams <steven.adams.95@hotmail.com>
2025-11-14 17:31:10 +0100

    update setup

--- schedulers/partitions.sh
+++ schedulers/partitions.sh
@@ -12,24 +12,24 @@ export PYTHONPATH="$PWD${PYTHONPATH:+:$PYTHONPATH}"
 
 echo "START"
 
-# echo "Uniform"
-# python -m experiments.partitions --distribution Uniform --num_dims 2 --setting 0
-# python -m experiments.partitions --distribution Uniform --num_dims 2 --setting 1
-# python -m experiments.partitions --distribution Uniform --num_dims 2 --setting 2
-# python -m experiments.partitions --distribution Uniform --num_dims 3 --setting 0
+echo "Uniform"
+python -m experiments.partitions --distribution Uniform --num_dims 2 --setting 0
+python -m experiments.partitions --distribution Uniform --num_dims 2 --setting 1
+python -m experiments.partitions --distribution Uniform --num_dims 2 --setting 2
+python -m experiments.partitions --distribution Uniform --num_dims 3 --setting 0
 
-# python -m experiments.partitions --distribution Uniform --num_dims 100 --setting 0
-# python -m experiments.partitions --distribution Uniform --num_dims 100 --setting 1
+python -m experiments.partitions --distribution Uniform --num_dims 100 --setting 0
+python -m experiments.partitions --distribution Uniform --num_dims 100 --setting 1
 python -m experiments.partitions --distribution Uniform --num_dims 100 --setting 2
 
-# echo "Gaussians"
-# python -m experiments.partitions --distribution Gaussian --num_dims 2 --setting 0
-# python -m experiments.partitions --distribution Gaussian --num_dims 3 --setting 0
-# python -m experiments.partitions --distribution Gaussian --num_dims 10 --setting 0
-# python -m experiments.partitions --distribution Gaussian --num_dims 100 --setting 0
+echo "Gaussians"
+python -m experiments.partitions --distribution Gaussian --num_dims 2 --setting 0
+python -m experiments.partitions --distribution Gaussian --num_dims 3 --setting 0
+python -m experiments.partitions --distribution Gaussian --num_dims 10 --setting 0
+python -m experiments.partitions --distribution Gaussian --num_dims 100 --setting 0
 
-# echo "GaussianMixture"
-# python -m experiments.partitions --distribution GaussianMixture --num_dims 2 --setting 0
-# python -m experiments.partitions --distribution GaussianMixture --num_dims 3 --setting 0
+echo "GaussianMixture"
+python -m experiments.partitions --distribution GaussianMixture --num_dims 2 --setting 0
+python -m experiments.partitions --distribution GaussianMixture --num_dims 3 --setting 0
 
 echo "FINNISHED"

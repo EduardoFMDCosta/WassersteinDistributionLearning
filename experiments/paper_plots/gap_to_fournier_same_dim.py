ed55a7d7426bba741fcab3afb62a298ec8e9a82c
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-11-22 13:03:50 +0100

    add gap to fournier same dimension

--- experiments/paper_plots/gap_to_fournier_same_dim.py
+++ experiments/paper_plots/gap_to_fournier_same_dim.py
@@ -2,7 +2,7 @@ import torch
 import matplotlib.pyplot as plt
 
 from configs.handlers import parse_arguments
-from experiments.utils import data_driven_radii_for_combinations
+from experiments.utils import data_driven_radii_for_combinations, fournier_radii_for_combinations
 
 plt.rcParams.update({
     "text.usetex": True,
@@ -28,8 +28,8 @@ if __name__ == '__main__':
     torch.manual_seed(0)
 
     args = parse_arguments(
-        distribution="GaussianMixture",
-        num_dims=3,
+        distribution="Gaussian",
+        num_dims=10,
         setting=0,
         num_samples_training=1000,
         beta=1e-6,
@@ -47,6 +47,7 @@ if __name__ == '__main__':
 
     (quantizations, data_driven_radii), _ = data_driven_radii_for_combinations(args, combinations=combinations,
                                                                                generate_partition_if_missing=True)
+    fournier_radii = fournier_radii_for_combinations(args, combinations)
 
     # Plot inflection curves
     fig, ax = plt.subplots(figsize=(6, 4))
@@ -56,12 +57,13 @@ if __name__ == '__main__':
         for M in M_options:
             key = (args.num_samples_training, N, M)
             radius = data_driven_radii.data[key].radius
-            y_vals.append(radius)
+            fournier_radius = fournier_radii.data[key]
+            y_vals.append(radius / fournier_radius)
 
         ax.plot(M_options, y_vals, marker="o", label=sci_label(N))
 
     ax.set_xlabel(r"$M$")
-    ax.set_ylabel("Our bound")
+    ax.set_ylabel("Our bound / Fournier")
     ax.grid(True, linestyle="--", alpha=0.4)
     ax.legend(title=r"$N$", loc="best")
 

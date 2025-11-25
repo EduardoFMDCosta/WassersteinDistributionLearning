4c25a8f27a978331558b6b61ba7a8c98e9054942
sjladams <steven.adams.95@hotmail.com>
2025-11-25 21:50:30 +0100

    update setup

--- schedulers/generate_results_gaussian.sh
+++ schedulers/generate_results_gaussian.sh
@@ -10,14 +10,15 @@ conda activate concentration_inequalities
 # optional but explicit
 export PYTHONPATH="$PWD${PYTHONPATH:+:$PYTHONPATH}"
 
+
+for method in diagonal_constrained_tp triangle_inequality_vertex joint_diagonal_milp joint_optimization_milp joint_full_expansion_milp; do
+echo "------- method = ${method} ---------"
+
 for num_samples_training in 1000 5000; do
 echo "---------------- num training samples: ${num_samples_training} -------------------"
 
 for rho in 1 2; do
 echo "---------------- W${rho} -------------------"
-
-for method in diagonal_constrained_tp triangle_inequality_vertex; do
-echo "------- method = ${method} ---------"
 
 echo "-- Gaussian --"
     echo "dim = 2, setting = -1"
--- schedulers/generate_results_uniform.sh
+++ schedulers/generate_results_uniform.sh
@@ -11,14 +11,14 @@ conda activate concentration_inequalities
 export PYTHONPATH="$PWD${PYTHONPATH:+:$PYTHONPATH}"
 
 
+for method in diagonal_constrained_tp triangle_inequality_vertex joint_diagonal_milp joint_optimization_milp joint_full_expansion_milp; do
+echo "------- method = ${method} ---------"
+
 for num_samples_training in 1000 5000; do
 echo "---------------- num training samples: ${num_samples_training} -------------------"
 
 for rho in 1 2; do
 echo "---------------- W${rho} -------------------"
-for method in diagonal_constrained_tp triangle_inequality_vertex; do
-
-echo "------- method = ${method} ---------"
 
 echo "-- Uniform --"
     echo "dim = 2, setting = 0"

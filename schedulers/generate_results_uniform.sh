144dca258418266822187aaa187e4a7f8e76a7c6
sjladams <steven.adams.95@hotmail.com>
2025-11-27 20:28:55 +0100

    update setup

--- schedulers/generate_results_uniform.sh
+++ schedulers/generate_results_uniform.sh
@@ -11,10 +11,10 @@ conda activate concentration_inequalities
 export PYTHONPATH="$PWD${PYTHONPATH:+:$PYTHONPATH}"
 
 
-for method in triangle_inequality_vertex joint_diagonal_milp; do
+for method in triangle_inequality_vertex; do # joint_diagonal_milp
 echo "------------------------------------------------- method = ${method} -----------------------------------------------------"
 
-for num_samples_training in 5000; do
+for num_samples_training in 5000 1000; do
 echo "---------------- num training samples: ${num_samples_training} -------------------"
 
 for rho in 1 2; do
@@ -36,41 +36,41 @@ echo "-- Uniform --"
     echo "dim = 2, setting = 4"
     python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 4 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
 
-    echo "dim = 3, setting = 0"
-    python -m experiments.generate_results --distribution Uniform --num_dims 3 --setting 0 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
+    # echo "dim = 3, setting = 0"
+    # python -m experiments.generate_results --distribution Uniform --num_dims 3 --setting 0 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
 
-    echo "dim = 10, setting = 0"
-    python -m experiments.generate_results --distribution Uniform --num_dims 10 --setting 0 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
+    # echo "dim = 10, setting = 0"
+    # python -m experiments.generate_results --distribution Uniform --num_dims 10 --setting 0 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
 
-    echo "dim = 10, setting = 1"
-    python -m experiments.generate_results --distribution Uniform --num_dims 10 --setting 1 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
+    # echo "dim = 10, setting = 1"
+    # python -m experiments.generate_results --distribution Uniform --num_dims 10 --setting 1 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
 
-    echo "dim = 25, setting = 0"
-    python -m experiments.generate_results --distribution Uniform --num_dims 25 --setting 0 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
+    # echo "dim = 25, setting = 0"
+    # python -m experiments.generate_results --distribution Uniform --num_dims 25 --setting 0 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
 
-    echo "dim = 25, setting = 1"
-    python -m experiments.generate_results --distribution Uniform --num_dims 25 --setting 1 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
+    # echo "dim = 25, setting = 1"
+    # python -m experiments.generate_results --distribution Uniform --num_dims 25 --setting 1 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
 
-    echo "dim = 50, setting = 0"
-    python -m experiments.generate_results --distribution Uniform --num_dims 50 --setting 0 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
+    # echo "dim = 50, setting = 0"
+    # python -m experiments.generate_results --distribution Uniform --num_dims 50 --setting 0 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
 
-    echo "dim = 50, setting = 1"
-    python -m experiments.generate_results --distribution Uniform --num_dims 50 --setting 1 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
+    # echo "dim = 50, setting = 1"
+    # python -m experiments.generate_results --distribution Uniform --num_dims 50 --setting 1 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
 
-    echo "dim = 75, setting = 0"
-    python -m experiments.generate_results --distribution Uniform --num_dims 75 --setting 0 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
+    # echo "dim = 75, setting = 0"
+    # python -m experiments.generate_results --distribution Uniform --num_dims 75 --setting 0 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
 
-    echo "dim = 75, setting = 1"
-    python -m experiments.generate_results --distribution Uniform --num_dims 75 --setting 1 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
+    # echo "dim = 75, setting = 1"
+    # python -m experiments.generate_results --distribution Uniform --num_dims 75 --setting 1 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
 
-    echo "dim = 100, setting = 0"
-    python -m experiments.generate_results --distribution Uniform --num_dims 100 --setting 0 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
+    # echo "dim = 100, setting = 0"
+    # python -m experiments.generate_results --distribution Uniform --num_dims 100 --setting 0 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
 
-    echo "dim = 100, setting = 1"
-    python -m experiments.generate_results --distribution Uniform --num_dims 100 --setting 1 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
+    # echo "dim = 100, setting = 1"
+    # python -m experiments.generate_results --distribution Uniform --num_dims 100 --setting 1 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
 
-    echo "dim = 100, setting = 2"
-    python -m experiments.generate_results --distribution Uniform --num_dims 100 --setting 2 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
+    # echo "dim = 100, setting = 2"
+    # python -m experiments.generate_results --distribution Uniform --num_dims 100 --setting 2 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training"
 
 
 done        

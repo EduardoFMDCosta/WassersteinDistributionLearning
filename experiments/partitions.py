7af49827d6401db2426fd9127c759303d560a341
sjladams <steven.adams.95@hotmail.com>
2025-12-22 17:25:39 +0100

    fix: timing saving

--- experiments/partitions.py
+++ experiments/partitions.py
@@ -148,7 +148,7 @@ def generate_partitions(
 
     # SAVE timer
     if args.save:
-        pickle_dump(time_logger, os.path.join(args.timing_dir, f"partitions_{args.partitions_file}"))
+        pickle_dump(time_logger, args.partitions_file.replace(".pickle", "_timing.pickle"))
 
     return partitions
 

b8899ec39a3f14555589c2a3f775447c0a113f12
sjladams <steven.adams.95@hotmail.com>
2025-12-23 08:40:08 +0100

    update schedulers

--- schedulers/generate_results_uniform.sh
+++ schedulers/generate_results_uniform.sh
@@ -23,21 +23,6 @@ for rho in 1; do
 echo "---------------- W${rho} -------------------"
 
 echo "-- Uniform --"
-    echo "dim = 2, setting = 0"
-    python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 0 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training" --random_seed "$seed"  
-
-    echo "dim = 2, setting = 1"
-    python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 1 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training" --random_seed "$seed"  
-
-    echo "dim = 2, setting = 2"
-    python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 2 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training" --random_seed "$seed"  
-
-    echo "dim = 2, setting = 3"
-    python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 3 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training" --random_seed "$seed"  
-
-    echo "dim = 2, setting = 4"
-    python -m experiments.generate_results --distribution Uniform --num_dims 2 --setting 4 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training" --random_seed "$seed"
-
     echo "dim = 10, setting = 0"
     python -m experiments.generate_results --distribution Uniform --num_dims 10 --setting 0 --wasserstein_order "$rho" --method "$method" --num_samples_training "$num_samples_training" --random_seed "$seed"
 
--- schedulers/partitions_uniform.sh
+++ schedulers/partitions_uniform.sh
@@ -13,43 +13,20 @@ export PYTHONPATH="$PWD${PYTHONPATH:+:$PYTHONPATH}"
 echo "START"
 
 
-echo "--------- num_dims = 2 -------------"
-
-for seed in 0 1 2 3 4 5 6 7 8 9; do
-echo "---------------- Random seed = ${seed} -------------------"
-
-python -m experiments.partitions --distribution Uniform --num_dims 2 --setting 0 --random_seed "$seed"
-python -m experiments.partitions --distribution Uniform --num_dims 2 --setting 1 --random_seed "$seed"
-python -m experiments.partitions --distribution Uniform --num_dims 2 --setting 2 --random_seed "$seed"
-python -m experiments.partitions --distribution Uniform --num_dims 2 --setting 3 --random_seed "$seed"
-python -m experiments.partitions --distribution Uniform --num_dims 2 --setting 4 --random_seed "$seed"
-
-done
-
-
 echo "--------- num_dims > 2 -------------"
 
 for seed in 0 1 2 3 4 5 6 7 8 9; do
 echo "---------------- Random seed = ${seed} -------------------"
 
-
-python -m experiments.partitions --distribution Uniform --num_dims 3 --setting 0 --random_seed "$seed"
-
 python -m experiments.partitions --distribution Uniform --num_dims 10 --setting 0 --random_seed "$seed"
-python -m experiments.partitions --distribution Uniform --num_dims 10 --setting 1 --random_seed "$seed"
 
 python -m experiments.partitions --distribution Uniform --num_dims 25 --setting 0 --random_seed "$seed"
-python -m experiments.partitions --distribution Uniform --num_dims 25 --setting 1 --random_seed "$seed"
 
 python -m experiments.partitions --distribution Uniform --num_dims 50 --setting 0 --random_seed "$seed"
-python -m experiments.partitions --distribution Uniform --num_dims 50 --setting 1 --random_seed "$seed"
 
 python -m experiments.partitions --distribution Uniform --num_dims 75 --setting 0 --random_seed "$seed"
-python -m experiments.partitions --distribution Uniform --num_dims 75 --setting 1 --random_seed "$seed"
 
 python -m experiments.partitions --distribution Uniform --num_dims 100 --setting 0 --random_seed "$seed"
-python -m experiments.partitions --distribution Uniform --num_dims 100 --setting 1 --random_seed "$seed"
-python -m experiments.partitions --distribution Uniform --num_dims 100 --setting 2 --random_seed "$seed"
 
 done
 echo "FINISHED"

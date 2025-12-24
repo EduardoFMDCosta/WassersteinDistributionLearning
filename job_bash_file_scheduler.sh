4c314bd5ba36020ae733e0f6839ce8c2dbbac789
sjladams <steven.adams.95@hotmail.com>
2025-12-24 15:23:43 +0100

    update

--- job_bash_file_scheduler.sh
+++ job_bash_file_scheduler.sh
@@ -4,14 +4,16 @@ FILE_NAME=$1
 
 #chmod -R +x /home/sjladams/projects/concentration_inequalities
 
-cat > temp_job_script.sh <<EOF
-#
+cat > temp_job_script.sh <<'EOF'
+#!/bin/sh
+set -eu
+
 # requested nr nodes and cores:
 #PBS -l nodes=1:ppn=8,mem=32gb
-#
+
 # name job:
 #PBS -N ${FILE_NAME}
-#
+
 # names output and error files:
 #PBS -j oe
 #PBS -o logging/${FILE_NAME}.pbs.txt
@@ -31,12 +33,13 @@ cd $PBS_O_HOME
 source miniconda3/bin/activate
 conda activate concentration_inequalities
 
-cd projects/ConcentrationInequalities
-LOG="logging/${FILE_NAME}.txt"
+# go to project
+cd "$PBS_O_HOME/projects/ConcentrationInequalities"
 mkdir -p logging
+LOG="logging/${FILE_NAME}.txt"
 
-# --- vmstat logging ---
-JOBID="${PBS_JOBID:-${FILE_NAME}.$$}"
+# --- vmstat logging on node-local disk ---
+JOBID="${PBS_JOBID:-__JOBNAME__.$$}"
 TMPDIR="/var/tmp/$JOBID"
 mkdir -p "$TMPDIR"
 
@@ -44,13 +47,19 @@ vmstat 1 > "$TMPDIR/vmstat.log" &
 VMSTAT_PID=$!
 # --- end vmstat logging ---
 
-stdbuf -oL -eL bash schedulers/${FILE_NAME}.sh >> "$LOG" 2>&1
+# execute scheduler file:
+stdbuf -oL -eL bash "schedulers/__JOBNAME__.sh" >> "$LOG" 2>&1
 
-kill "\$VMSTAT_PID" 2>/dev/null || true
-cp "\$TMPDIR/vmstat.log" "logging/${FILE_NAME}.vmstat.txt" || true
-
+# cleanup + copy back
+kill "$VMSTAT_PID" 2>/dev/null || true
+cp "$TMPDIR/vmstat.log" "logging/__JOBNAME__.vmstat.txt" || true
+rm -rf "$TMPDIR" 2>/dev/null || true
 
 EOF
 
-# submit the job script
-qsub temp_job_script.sh
+# Replace placeholders with the job name (FILE_NAME) in the generated script
+# (sed is safe here because __JOBNAME__ is a fixed token)
+sed -i "s/__JOBNAME__/${FILE_NAME}/g" temp_job_script.sh
+
+# submit the job script, also export FILE_NAME (optional, but harmless)
+qsub -v FILE_NAME="${FILE_NAME}" temp_job_script.sh

b5bfd911b1386ae106170a0d91a0d3fd6bf3b0d3
sjladams <steven.adams.95@hotmail.com>
2025-12-30 09:16:32 +0100

    update setup

--- experiments/generate_results.py
+++ experiments/generate_results.py
@@ -24,8 +24,8 @@ if __name__ == '__main__':
     # N_options = [1000, 2500, 5000, 7500, 10000, 100000, 1000000]
     # M_options = [5, 20, 30, 40, 50, 75, 100, 150, 200, 500, 1000]
 
-    N_options = [1000, 2500, 5000, 7500, 10000, 100000, 1000000]
-    M_options = [5, 20, 30, 40, 50, 75, 100, 150]
+    N_options = [1000, 2500]
+    M_options = [5, 20, 30, 40, 50, 75, 100, 150, 200, 500, 1000]
 
     data_driven_radii, time_logger = data_driven_radii_for_combinations(
         args, 

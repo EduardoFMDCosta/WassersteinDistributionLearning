1a714e09c89b60ae1150eac7f6a31e7008d481d4
sjladams <steven.adams.95@hotmail.com>
2026-01-08 10:57:07 +0100

    refactor: improve multiscale

--- experiments/paper_plots/multiscale.py
+++ experiments/paper_plots/multiscale.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from configs.construct import get_support_assumption, get_distribution
 from configs.handlers import parse_arguments, process_args
 from experiments.partitions import get_partition
-from experiments.utils import load_list_of_data_driven_radii, fournier_radii_for_combinations, load_quantization
+from experiments.utils import load_list_of_data_driven_radii, load_list_of_empirical_radii, load_quantization
 
 from plotting.utils_plot import set_style, convert_to_sci_notation
 
@@ -24,9 +24,8 @@ def main(args):
     
     random_seed_options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    M_options = [5, 20, 30, 40, 50, 75, 100, 150, 200, 500, 1000]
-
-    metric = {1: "euclidean", 2: "sqeuclidean"}
+    # M_options = [5, 20, 30, 40, 50, 75, 100, 150, 200, 500, 1000]
+    M_options = [5, 20, 30]
 
     fig, ax = plt.subplots(figsize=(6, 4))
     cmap = plt.cm.coolwarm
@@ -36,53 +35,34 @@ def main(args):
         args.setting = setting
         args = process_args(args)
 
-        distribution = get_distribution(**vars(args))
-
         combinations = [(args.num_samples_training, args.num_samples, M) for M in M_options]
 
         data = load_list_of_data_driven_radii(args, combinations, random_seed_options)
-        fournier_radii = fournier_radii_for_combinations(args, [(combi[1], combi[2]) for combi in combinations])
-
-        M_options_plot = [key[2] for key in data.keys(N=args.num_samples, N_train=args.num_samples_training)]
-        radii, radii_minus, radii_plus = list(), list(), list()
-        for M in M_options_plot:
-            key = (args.num_samples_training, args.num_samples, M)
-
-            if key in fournier_radii.keys():
-                radii.append(data.mean_radius_at(key))
-                radii_minus.append((data.mean_radius_at(key) - data.std_radius_at(key)))
-                radii_plus.append((data.mean_radius_at(key) + data.std_radius_at(key)))
-            else:
-                continue
-
-        empirical = list()
-        for M in M_options_plot:
-            partition = get_partition(args=args, num_samples=args.num_samples_training, num_clusters=M)
-            quantization = load_quantization(args=args, partition=partition, N=args.num_samples)
-
-            emp_dist = distribution.sample((100000,))
-
-            radius_quantization = ot.solve_sample(X_a=emp_dist, X_b=quantization.locs, b=quantization.probs,
-                                                  metric=metric[args.wasserstein_order]).value.pow(1 / args.wasserstein_order).item()
-
-            empirical.append(radius_quantization)
-
-        M_options_plot, radii = torch.as_tensor(M_options_plot), torch.as_tensor(radii)
-        radii_minus, radii_plus = torch.as_tensor(radii_minus), torch.as_tensor(radii_plus)
-        empirical = torch.as_tensor(empirical)
+        M_options_plot = torch.as_tensor([key[2] for key in data.keys()])
         idx = M_options_plot.argsort()
 
-        ax.plot(M_options_plot[idx], radii[idx], label=rf"${convert_to_sci_notation(args.variance**0.5)}$", color=color, marker="o")
+        ax.plot(M_options_plot[idx], data.mean_radius[idx], label=rf"${convert_to_sci_notation(args.variance**0.5)}$", color=color, marker="o")
         ax.fill_between(
             M_options_plot[idx],
-            radii_minus[idx],
-            radii_plus[idx],
+            data.mean_radius[idx] - data.std_radius[idx],
+            data.mean_radius[idx] + data.std_radius[idx],
             alpha=0.2,
             color=color
         )
 
-        ax.plot(M_options_plot[idx], empirical[idx], color=color, marker="o", linestyle="--")
+        data_emp = load_list_of_empirical_radii(args, combinations, random_seed_options, save=True)
 
+        M_emp_options_plot = torch.as_tensor([key[2] for key in data_emp.keys()])
+        idx_emp = M_emp_options_plot.argsort()
+
+        ax.plot(M_emp_options_plot[idx_emp], data_emp.mean_radius[idx_emp], label=rf"${convert_to_sci_notation(args.variance**0.5)}$", color=color, marker="o", linestyle="--")
+        ax.fill_between(
+            M_emp_options_plot[idx_emp],
+            data_emp.mean_radius[idx_emp] - data_emp.std_radius[idx_emp],
+            data_emp.mean_radius[idx_emp] + data_emp.std_radius[idx_emp],
+            alpha=0.2,
+            color=color
+        )
 
     ax.set_xlabel(r"Support size of $\widehat{\mathbb{P}}$")
     ax.set_ylabel("Our bound")

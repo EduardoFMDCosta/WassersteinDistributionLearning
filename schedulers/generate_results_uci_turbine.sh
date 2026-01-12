0a917b905a7e79fa3e188cad597a461a5d6d47c6
sjladams <steven.adams.95@hotmail.com>
2026-01-12 23:05:40 +0100

    update setup

---
+++ schedulers/generate_results_uci_turbine.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Initialize conda
+source /home/sjladams/miniconda3/etc/profile.d/conda.sh
+
+# Activate your environment
+conda activate concentration_inequalities
+
+# optional but explicit
+export PYTHONPATH="$PWD${PYTHONPATH:+:$PYTHONPATH}"
+
+for seed in 0 1 2 3 4 5 6 7 8 9; do
+echo "-------------------------------------------- Random seed = ${seed} -----------------------------------------------"
+
+for method in triangle_inequality_vertex; do
+echo "------------------------------------------------- method = ${method} -----------------------------------------------------"
+
+for num_samples_training in 1000 5000; do
+echo "---------------- num training samples: ${num_samples_training} -------------------"
+
+for rho in 2; do
+echo "---------------- W${rho} -------------------"
+
+echo "-- UCI-Turbine --"
+    echo "dim = 11, setting = 0"
+    python -m experiments.generate_results --distribution UCI-Turbine --num_dims 11 --setting 0 --wasserstein_order "$rho" --method "$method"  --num_samples_training "$num_samples_training" --random_seed "$seed"
+
+done        
+done
+done
+done
+
+echo "Finished"
+
